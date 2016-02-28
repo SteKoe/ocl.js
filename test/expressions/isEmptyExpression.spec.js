be0f1f56e3c8c9598ec45385600661128b461b79
Stephan Köninger <github@stekoe.de>
2016-02-28 13:43:39 +0100

    feat(isEmptyExpression): add ->isEmpty() method for collections

---
+++ test/expressions/isEmptyExpression.spec.js
@@ -0,0 +1,19 @@
+const should = require('should');
+
+import AttributeCallExpression from '../../lib/expressions/attributeCallExpression'
+import IsEmptyExpression from '../../lib/expressions/isEmptyExpression'
+
+describe.only('AttributeCallExpression', () => {
+    it('should evaluate isEmtpy when empty', () => {
+        const ne = new IsEmptyExpression(new AttributeCallExpression('self', 'children'));
+
+        ne.evaluate({name: 'Otto'}).should.be.true;
+        ne.evaluate({name: 'Otto', children: []}).should.be.true;
+    });
+
+    it('should evaluate isEmpty when not empty', () => {
+        const ne = new IsEmptyExpression(new AttributeCallExpression('self', 'children'));
+
+        ne.evaluate({name: 'Otto', children: [1]}).should.be.true;
+    });
+});
