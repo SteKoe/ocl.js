308beee1c3a0a95ceba48d33dd18e30aada85e7a
Stephan Köninger <github@stekoe.de>
2016-03-20 22:54:13 +0100

    add new examples

--- examples/examples.js
+++ examples/examples.js
@@ -1,13 +1,5 @@
 import OclParser from '../lib/oclParser';
 
-class Person {
-    constructor(name, age) {
-        this.name = name;
-        this.age = age;
-        this.typeName = 'Person';
-    }
-}
-
 class Example {
     constructor(id) {
         this.id = id;
@@ -19,81 +11,62 @@ class Example {
     }
 
     run(fn) {
-        const elementById = document.getElementById(`example${this.id}-result`);
-        if (elementById) {
-            elementById.innerText = fn() ? 'valid' : 'invalid';
-            elementById.innerText += ` expected ${this.expected ? 'valid' : 'invalid'}!`
+        const elementById = document.getElementById(`example${this.id}`);
+        const resultTag = elementById.getElementsByClassName('result')[0];
+        if (resultTag) {
+            resultTag.innerText = fn() ? 'valid' : 'invalid';
+            resultTag.innerText += ` expected ${this.expected ? 'valid' : 'invalid'}!`
         }
     }
 }
 
+// ======================================
+class Person {
+    constructor(age) {
+        this.typeName = 'Person';
+        this.age = age;
+        this.fleet = [];
+    }
+}
+
+class Car {
+    constructor(color) {
+        this.typeName = 'Car';
+        this.color = color;
+    }
+}
+
 new Example(1).expect(true).run(() => {
-    var person = new Person('Stephan', 29);
+    var person = new Person(29);
+    var car = new Car('red');
+    car.owner = person;
 
     var oclExpression = [
-        'context Person',
-        '   inv: self.parent <> self'
+        'context Car',
+        '   inv: self.owner.age >= 18'
     ].join('\n');
 
     var oclRule = new OclParser(oclExpression).parse();
-    return oclRule.evaluate(person)
+    return oclRule.evaluate(car);
 });
 
 new Example(2).expect(true).run(() => {
-    var oclExpression = [
-        'context Person',
-        '   inv ChildrenAreAllYounger: self.children->forAll(c|c.age < self.age)'
-    ].join('\n');
+    var person = new Person(29);
+    var ferrari = new Car('red');
+    ferrari.owner = person;
+    var opel = new Car('silver');
+    opel.owner = person;
 
-    var mother = new Person("Henriette", 67);
-    mother.children = [
-        {name: 'Heike', age: 27},
-        {name: 'Swen', age: 47}
-    ];
-
-    const oclRule = new OclParser(oclExpression).parse();
-    return oclRule.evaluate(mother);
-});
-
-new Example(3).expect(false).run(() => {
-    var mother = new Person("Henriette", 67);
-    mother.children = [
-        {name: 'Heike', age: 27},
-        {name: 'Swen', age: 47},
-        {name: 'Heike', age: 30}
-    ];
+    person.fleet.push(ferrari);
+    person.fleet.push(opel);
+    person.fleet.push(ferrari);
+    person.fleet.push(opel);
 
     var oclExpression = [
-        'context Person',
-        '   inv: self.children->forAll(c1,c2| c1.name <> c2.name)'
+        'context Peson',
+        '   inv: self.fleet->size <= 3'
     ].join('\n');
 
     var oclRule = new OclParser(oclExpression).parse();
-    return oclRule.evaluate(mother);
-});
-
-new Example(5).expect(false).run(() => {
-    class MetaEntity {
-        constructor() {
-            this.typeName = 'MetaEntity';
-        }
-    }
-
-    const metaEntity = new MetaEntity();
-    metaEntity.name = "Employee";
-    metaEntity.isIntrinsic = true;
-    metaEntity.metaAttribute = [
-        {name: 'lastName', type: 'string'},
-        {name: 'firstName', type: 'float', isIntrinsic: true},
-        {name: 'dateOfBirth', type: 'date', isIntrinsic: true},
-        {name: 'formalQualification', type: 'string', isIntrinsic: true}
-    ];
-
-    var oclExpression = [
-        'context MetaEntity inv:',
-        '   self.isIntrinsic = true implies self.metaAttribute->forAll(a | a.isInstrinsic = true)'
-    ].join('\n');
-
-    var oclRule = new OclParser(oclExpression).parse();
-    return oclRule.evaluate(metaEntity);
+    return oclRule.evaluate(person);
 });
