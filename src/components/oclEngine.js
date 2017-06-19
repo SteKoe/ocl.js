11c6a750cf7bda2ccc83c5ca7be7ecdaadb1d3e6
Stephan <github@stekoe.de>
2017-06-19 18:07:25 +0200

    chore(doc): add doc and apply dist to fit new documentation

--- src/components/oclEngine.js
+++ src/components/oclEngine.js
@@ -1,7 +1,17 @@
-import {OclParser} from './parser/oclParser';
-import {Utils} from './utils';
+import {OclParser} from "./parser/oclParser";
+import {Utils} from "./utils";
 
+/**
+ * The OclEngine class is the main entry point to the OCL.js library.
+ *
+ * This class allows to add new OCL expressions as well as evaluating given objects agains the saved OCL expressions.
+ */
 export class OclEngine {
+    /**
+     * Static create method.
+     *
+     * @returns {OclEngine} a fresh new instance of the OclEngine
+     */
     static create() {
         return new OclEngine();
     }
@@ -10,14 +20,24 @@ export class OclEngine {
         this.oclExpressionsPerType = {};
     }
 
+    /**
+     * Register a new OCL expression.
+     *
+     * Each OCL expression is added to a map of OCL expressions where the context is the key:
+     * <pre>
+     *     context Person inv: ...
+     * </pre>
+     *
+     * Will be sorted into the map using "Person" as key. This allows faster lookup of OCL rules that should be applied
+     * when running OclEngine.evaluate.
+     *
+     * @param {String} oclExpression The OCL expression as string. It will be parsed and added to the list of existing OCL expressions.
+     * @returns {OclEngine} the current OclEngine object for chaining
+     */
     addOclExpression(oclExpression) {
         const parsedExpression = OclParser.parse(oclExpression);
         this._addOclExpressionForType(parsedExpression.targetType, parsedExpression);
         return this;
-    }
-
-    getOclExpressionsForType(type) {
-        return this.oclExpressionsPerType[type] || [];
     }
 
     _addOclExpressionForType(type, oclExpression) {
@@ -26,12 +46,28 @@ export class OclEngine {
         this.oclExpressionsPerType[type] = oclExpressions;
     }
 
+    /**
+     * For a fiven type returns all registered OCL expressions.
+     *
+     * @param {String} type The type to lookup OCL expressions for.
+     * @returns {Array} an array of {Expression}s.s
+     */
+    getOclExpressionsForType(type) {
+        return this.oclExpressionsPerType[type] || [];
+    }
+
+    /**
+     * This function actually evaluates the given object against the registered OCL expressions.
+     *
+     * @param {Object} obj The object which is going to be evaluated against registered OCL expressions.
+     * @returns {OclResult} a result object, which contains the actual result and other info @see OclResult
+     */
     evaluate(obj) {
         const type = Utils.getClassName(obj);
         const oclExpressionsForType = this.getOclExpressionsForType(type);
         oclExpressionsForType.forEach(e => e.evaluate(obj));
 
-        var namesOfFailedInvs = oclExpressionsForType
+        const namesOfFailedInvs = oclExpressionsForType
             .filter(e => e.evaluationResult === false)
             .map(e => e.invs)
             .reduce((p, c) => p.concat(c), [])
@@ -45,6 +81,12 @@ export class OclEngine {
     }
 }
 
+/**
+ * This class is a data class that is returned by OclEngine.evaluate method.
+ *
+ * It contains useful information about the evaluation result like the actual result (true / false) and in addition a list
+ * of all names of invariants that have failed.
+ */
 class OclResult {
     setResult(result) {
         this.result = result;
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     entry: "./index",
     output: {
         path: __dirname + "/dist",
-        filename: "ocl.js"
+        filename: "ocl.min.js"
     },
     module: {
         rules: [
@@ -22,6 +22,9 @@ module.exports = {
     plugins: [
         new webpack.DefinePlugin({
             'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: { warnings: false }
         })
     ]
 };
