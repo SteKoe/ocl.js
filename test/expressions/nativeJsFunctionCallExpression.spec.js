06cea26ff40d9a704b8c73207e19786301a45f6c
Stephan <github@stekoe.de>
2018-02-04 18:25:25 +0100

    chore(NativeJsFunctionCallExpression): add more tests

---
+++ test/expressions/nativeJsFunctionCallExpression.spec.js
@@ -0,0 +1,29 @@
+import {expect} from "chai";
+import {NativeJsFunctionCallExpression} from "../../lib/components/expressions/nativeJsFunctionCallExpression";
+import {VariableExpression} from "../../lib/components/expressions";
+
+describe('NativeJsFunctionCallExpression', () => {
+    const obj = {
+        getName() {
+            return "Name";
+        }
+    };
+
+    it('calls existing function on existing object', () => {
+        const variableExpression = new VariableExpression("self");
+        const functionCallExpression = new NativeJsFunctionCallExpression(variableExpression, 'getName');
+        expect(functionCallExpression.evaluate(obj)).to.equal("Name");
+    });
+
+    it('returns false for non existing function', () => {
+        const variableExpression = new VariableExpression("self");
+        const functionCallExpression = new NativeJsFunctionCallExpression(variableExpression, 'getNonExistingProperty');
+        expect(functionCallExpression.evaluate(obj)).to.be.false;
+    });
+
+    it('returns false for non existing object', () => {
+        const variableExpression = new VariableExpression("self.undefinedProperty");
+        const functionCallExpression = new NativeJsFunctionCallExpression(variableExpression, 'getNonExistingProperty');
+        expect(functionCallExpression.evaluate(obj)).to.be.false;
+    });
+});
