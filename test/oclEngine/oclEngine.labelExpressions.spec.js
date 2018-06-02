fdbda96fa76792739b8764b21114efea820ac897
Stephan Köninger <github@stekoe.de>
2018-06-02 15:32:25 +0200

    feat(#9): allow to add labels to expressions

---
+++ test/oclEngine/oclEngine.labelExpressions.spec.js
@@ -0,0 +1,24 @@
+'use strict';
+import {expect} from 'chai';
+import {OclEngine} from "../../lib/components/OclEngine";
+
+require('../../generator/oclParserGenerator');
+
+describe.only('OclEngine', function () {
+    it('should allow to label expressions', function () {
+        const oclEngine = OclEngine.create()
+            .addOclExpression('context Object inv: false', ['label-1'])
+            .addOclExpression('context Object inv: false', ['label-1'])
+            .addOclExpression('context Object inv: false', ['label-2']);
+    });
+
+    it('should allow to execute expressions having specific labels', function() {
+        const oclEngine = OclEngine.create()
+            .addOclExpression('context Object inv: false', ['label-1'])
+            .addOclExpression('context Object inv: false', ['label-1'])
+            .addOclExpression('context Object inv: false', ['label-2']);
+
+        expect(oclEngine.getOclExpressionsForType('Object', ['label-1']).length).to.eql(2);
+        expect(oclEngine.evaluate({}, ['label-1']).namesOfFailedInvs.length).to.eql(2);
+    })
+});
