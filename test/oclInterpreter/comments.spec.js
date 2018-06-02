a9b9d467b530e625b4f390717eeefff2295e1d2a
Stephan Köninger <github@stekoe.de>
2018-06-02 14:40:19 +0200

    feat(#7): adds tests for comments using # or //

---
+++ test/oclInterpreter/comments.spec.js
@@ -0,0 +1,50 @@
+'use strict';
+import {expect} from "chai";
+import {OclParser} from "../../lib/components/parser/OclParser";
+
+require('../../generator/oclParserGenerator');
+
+describe('Comments', () => {
+    it('should be allowed using // for single line comments', () => {
+        const oclExpression = `
+            // This is a valid comment
+            context Person inv:
+                self.children->exists(c|c.age > 20) 
+        `;
+
+        expect(() => OclParser.parse(oclExpression)).not.to.throw();
+    });
+
+    it('should fail when using // containing breaks', () => {
+        const oclExpression = `
+            // This is a valid comment
+              this is not
+            context Person inv:
+                self.children->exists(c|c.age > 20) 
+        `;
+
+        expect(() => OclParser.parse(oclExpression)).to.throw();
+    });
+
+    it('should be allowed using // for single line comments', () => {
+        const oclExpression = `
+            # This is a valid comment
+            context Person inv:
+                self.children->exists(c|c.age > 20) 
+        `;
+
+        expect(() => OclParser.parse(oclExpression)).not.to.throw();
+    });
+
+    it('should fail when using // containing breaks', () => {
+        const oclExpression = `
+            # This is a valid comment
+              this is not
+            context Person inv:
+                self.children->exists(c|c.age > 20) 
+        `;
+
+        expect(() => OclParser.parse(oclExpression)).to.throw();
+    });
+
+});
