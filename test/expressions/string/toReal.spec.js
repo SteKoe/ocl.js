977e32d51752ec01e2e717210df0d2a0d466fb3a
Stephan <github@stekoe.de>
2018-06-07 09:14:42 +0200

    chore(Expressions): implement toInteger() : Number and toReal() : Number

---
+++ test/expressions/string/toReal.spec.js
@@ -0,0 +1,10 @@
+import { expect } from "chai";
+import { expectOclRuleValidatesToTrue } from '../../matcher'
+
+describe('toReal', () => {
+    it('parses a string to real (float)', () => {
+        let obj = {};
+        let oclExpression = `context Object inv: "3.414"->toReal() = 3.414`;
+        expectOclRuleValidatesToTrue(oclExpression, obj);
+    });
+});
