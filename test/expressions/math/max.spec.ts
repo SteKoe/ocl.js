7a86af8d0da0183398c761b10797186492913e1d
Stephan Köninger <github@stekoe.de>
2018-06-14 20:52:32 +0200

    feat(MaxExpression): add Math->max(i : Integer)

---
+++ test/expressions/math/max.spec.ts
@@ -0,0 +1,24 @@
+import { OclEngine } from '../../../lib';
+import { expect } from 'chai';
+
+describe('Math->max', () => {
+    let oclEngine;
+
+    beforeEach(() => {
+        oclEngine = new OclEngine();
+    });
+
+    it('should find the greatest number', () => {
+        let result = oclEngine.evaluateQuery({}, oclEngine.createQuery('6->max(3)'));
+        expect(result).to.equal(6);
+
+        result = oclEngine.evaluateQuery({}, oclEngine.createQuery('6->max(5.2)'));
+        expect(result).to.equal(6);
+
+        result = oclEngine.evaluateQuery({}, oclEngine.createQuery('2.3->max(3)'));
+        expect(result).to.equal(3);
+
+        result = oclEngine.evaluateQuery({}, oclEngine.createQuery('2.3->max(5.2)'));
+        expect(result).to.equal(5.2);
+    });
+});
