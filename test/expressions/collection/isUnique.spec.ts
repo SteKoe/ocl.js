36e6b1b7068bea00365f66c01b338129e38c039d
Stephan Köninger <github@stekoe.de>
2018-12-17 23:12:33 +0100

    feat(isUnique): add ocl function isUnique

---
+++ test/expressions/collection/isUnique.spec.ts
@@ -0,0 +1,23 @@
+import { expectOclRuleValidatesToFalse, expectOclRuleValidatesToTrue } from '../../matcher';
+
+describe('Collection->isUnique', () => {
+    it('works using comparator', () => {
+        const fromOclSpec = [2.3, 5.2];
+
+        let oclExpression = 'context Object inv: self->isUnique(self > 3)';
+        expectOclRuleValidatesToTrue(oclExpression, fromOclSpec);
+
+        oclExpression = 'context Object inv: self->isUnique(self > 1)';
+        expectOclRuleValidatesToFalse(oclExpression, fromOclSpec);
+    });
+
+    it('works checking just for self', () => {
+        const stringArray = ['A', 'B', 'C', 'D'];
+
+        const oclExpression = 'context Object inv: self->isUnique(self)';
+        expectOclRuleValidatesToTrue(oclExpression, stringArray);
+
+        stringArray.push('A');
+        expectOclRuleValidatesToFalse(oclExpression, stringArray);
+    });
+});
